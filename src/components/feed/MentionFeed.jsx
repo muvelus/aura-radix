2f92d9b64f07be6adfb3634601c5ed88587c7ba2
Jason Noronha <noronha.jason1997@gmail.com>
2026-02-15 12:36:07 +0530

    Update MentionFeed.jsx

--- src/components/feed/MentionFeed.jsx
+++ src/components/feed/MentionFeed.jsx
@@ -19,9 +19,9 @@ import InlineReplyBox from "./InlineReplyBox";
 
 const PLATFORM_LOGOS = {
   reddit: "https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/reddit.svg",
-  youtube: "https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/instagram.svg",
-  twitter: "https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/x.svg",
+  youtube: "https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/youtube.svg",
   x: "https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/x.svg",
+  twitter: "https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/x.svg",
   instagram: "https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/instagram.svg",
 };
 
